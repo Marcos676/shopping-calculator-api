670bcb4a5e2272a3aab8095df8b898b3ac3b111f
Marcos676 <patino-marcos@outlook.com>
2025-12-07 19:30:03 -0300

    Agregar validaciones para el registro de usuarios, incluyendo nombre, email y contraseña

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import { userList, createUser } from '../controllers/usersController.js'
+import registerUserValidator from '../validations/registerUserValidator.js'
 
 router.get('/', userList)
-router.post('/register', createUser)
+router.post('/register', registerUserValidator, createUser)
 
 export default router ;
