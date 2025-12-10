9377dfeceef9b6152e9557cc511b8ce90649d83b
Marcos676 <patino-marcos@outlook.com>
2025-12-09 22:44:06 -0300

    Mejorar validaciones de registro de usuario y ajustar formato de código en controladores

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,39 +1,53 @@
-import { User, ShoppingList } from '../database/models/index.js';
-import { validationResult } from 'express-validator';
-
+import { User, ShoppingList } from "../database/models/index.js";
+import { validationResult } from "express-validator";
 
 const userList = async (req, res) => {
-    try {
-        const users = await User.findAll({ attributes: ['id', 'name', 'email'], include: [{ model: ShoppingList, as: 'shoppingLists', attributes: ['id', 'name', 'product_list', 'total_discount', 'final_price'] }] });
-        res.json(users);
-    } catch (error) {
-        console.error('Error al obtener usuarios:', error);
-        res.status(500).json({ error: 'Error al obtener usuarios' });
-    }
-}
+  try {
+    const users = await User.findAll({
+      attributes: ["id", "name", "email"],
+      include: [
+        {
+          model: ShoppingList,
+          as: "shoppingLists",
+          attributes: [
+            "id",
+            "name",
+            "product_list",
+            "total_discount",
+            "final_price",
+          ],
+        },
+      ],
+    });
+    res.json(users);
+  } catch (error) {
+    console.error("Error al obtener usuarios:", error);
+    res.status(500).json({ error: "Error al obtener usuarios" });
+  }
+};
 
-const createUser = async (req,res) => {
+const createUser = async (req, res) => {
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.json({
+      errors: errors.mapped(),
+    });
+  }
 
-    console.log(validationResult(req));
-    return res.json(validationResult(req))
-    
-
-    console.log(req.body, "<-- Respuesta de req.body")
-    let { name, email, password} = req.body;
-    try {
-        const user = await User.create({
-            name: name.trim(),
-            email: email.trim(),
-            password: password
-        });
-        console.log(user.toJSON());
-        return res.json({id: user.id, name: user.name, email: user.email})
-        
-    } catch (error) {
-        console.error('Error al obtener usuarios:', error);
-        res.status(500).json({ error: 'Error al crear el usuario' });
-    }
-    
-}
+  //console.log(req.body, "<-- Respuesta de req.body");
+  let { name, email, password } = req.body;
+  try {
+    const user = await User.create({
+      name: name.trim(),
+      email: email.trim(),
+      password: password,
+    });
+    //console.log(user.toJSON());
+    return res.json({ id: user.id, name: user.name, email: user.email });
+  } catch (error) {
+    console.error("Error al crear usuario:", error);
+    res.status(500).json({ error: "Error al crear el usuario" });
+  }
+};
 
 export { userList, createUser };
--- src/validations/registerUserValidator.js
+++ src/validations/registerUserValidator.js
@@ -1,28 +1,43 @@
+import { User } from "../database/models/index.js";
 import { check, body } from "express-validator";
 
 const validations = [
-  check("name").notEmpty().withMessage("Se requiere un nombre de usuario"),
+  check("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Se requiere un nombre de usuario"),
   body("name")
-    .custom((value) => {
-      let result = value.trim().length < 31 ? true : false;
-      return result;
+    .custom((value) => value.trim().length < 31)
+    .withMessage("Solo puede tener hasta 30 caracteres"), //Menos de 30 caracteres
+  body("name")
+    .custom(async (value) => {
+      const user = await User.findOne({ where: { name: value.trim() } });
+      if (user) {
+        throw new Error("Error en la busqueda de usuario");
+      }
     })
-    .withMessage("Puede tener hasta 30 caracteres"), //Menos de 30 caracteres
+    .withMessage("Este nombre ya existe"),
   check("email").notEmpty().withMessage("Se requiere un email"),
-  check("email").isEmail("El email debe ser válido"),
+  check("email").isEmail().withMessage("El email debe ser válido"),
+    body("email")
+    .custom(async (value) => {
+      const user = await User.findOne({ where: { email: value.trim() } });
+      if (user) {
+        throw new Error("Error en la busqueda de usuario");
+      }
+    })
+    .withMessage("Este email ya existe"),
   body("password")
     .custom((value) => {
       let regExPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/; // Condiciones de la expresion: al menos una letra minúscula - al menos una letra mayúscula - al menos un dígito - al menos 8 caracteres
-      let result = regExPass.test(value) ? true : false;
-      return result;
+      return regExPass.test(value);
     })
     .withMessage(
       "La contraseña debe tener:\n- Al menos una letra minúscula\n - Al menos una letra mayúscula\n - Al menos un dígito\n - Al menos 8 caracteres"
     ),
-  body("confirmacion").custom((value, { req }) => {
-    let result = value === req.body.password ?  true : false;
-    return result
-  }).withMessage('Las contraseñas no coinciden')
+  body("confirm")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Las contraseñas no coinciden"),
 ];
 
 export default validations;
